42638e0d4d30ba61ec9cb1ceea4c708c21aa132b
asifsha <asif-sharif@hotmail.com>
2019-07-18 00:00:41 +0500

    Menu button added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,43 @@
-import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Component } from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.less"],
   animations: [
-    trigger('slideInOut', [
-      state('in', style({
-        transform: 'translate3d(0,0,0)'
-      })),
-      state('out', style({
-        transform: 'translate3d(100%, 0, 0)'
-      })),
-      transition('in => out', animate('400ms ease-in-out')),
-      transition('out => in', animate('400ms ease-in-out'))
-    ]),
+    trigger("slideInOut", [
+      state(
+        "in",
+        style({
+          transform: "translate3d(0,0,0)"
+        })
+      ),
+      state(
+        "out",
+        style({
+          transform: "translate3d(100%, 0, 0)"
+        })
+      ),
+      transition("in => out", animate("400ms ease-in-out")),
+      transition("out => in", animate("400ms ease-in-out"))
+    ])
   ]
 })
 export class AppComponent {
-  title = 'angular-starter-kit';
+  title = "angular-starter-kit";
 
-  menuState:string = 'out';
+  iconSource = "../assets/images/menu-icon.png";
 
-  toggleMenu(){
-    this.menuState = this.menuState === 'out' ? 'in' : 'out';
+  menuState: string = "out";
+
+  toggleMenu() {
+    this.menuState = this.menuState === "out" ? "in" : "out";
   }
-  
-
 }
